abe58727c3c49d576c0912de3c55c8ee7be3ea9e
Theo Weller <weller.theodor@gmail.com>
2019-03-14 20:20:42 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+class Song
+attr_accessor(:name, :artist, :genre)
+@@count = 0
+@@artists = []
+@@genres = []
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@artists << artist
+  @@genres << genre
+  @@count += 1
+end
+
+def self.count
+@@count
+end
+
+def self.artists
+  @@artists.uniq
+end
+
+def self.genres
+@@genres.uniq
+end
+#
+def self.genre_count
+  result = {}
+  genre_count = 0
+  @@genres.each do |genre|
+    result[genre] ||= result.uniq.include?(genre)
+
+end
+
+binding.pry
+end
+
+
+end
